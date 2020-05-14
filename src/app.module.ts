d219c8ee246e02d8bb5d6ee207b65e0cf2782765
Arkadiusz Osoba <arkadiuszosoba@gmail.com>
2020-05-14 15:17:59 +0200

    Remove usused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [GraphQLModule.forRoot({
     autoSchemaFile: 'schema.gql'
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+  })]
 })
 export class AppModule {}
