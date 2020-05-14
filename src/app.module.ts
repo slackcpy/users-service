118a208e66dcc6e99e49cccc75c826ce3ff1ebb8
Arkadiusz Osoba <arkadiuszosoba@gmail.com>
2020-05-14 15:16:30 +0200

    Add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: 'schema.gql'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
