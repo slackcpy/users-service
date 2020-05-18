fbb3bf923ebd7f8fcc150d32d3d417b658361ef9
Arkadiusz Osoba <arkadiuszosoba@gmail.com>
2020-05-18 08:40:27 +0200

    Update port config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { env } from 'process';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
@@ -6,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule   
   );
-  app.listen(3000);
+  app.listen(env.PORT);
 }
 
 bootstrap();
